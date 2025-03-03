8109a0d5357ac640cf973814a9c817aece73591e
Jhun Von Jave <aujscmamaidjhunvonjave@gmail.com>
2025-03-03 20:29:05 +0800

    update again

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
-
+import { MyContext } from "../../App";
 const NotFound = () => {
+
+  const context = useContext(MyContext);
+
+  useEffect(() => {
+    context.setIsHideComponents(false);
+    window.scrollTo(0, 0);
+  }, [context]);
+
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>404 - Page Not Found</h1>
