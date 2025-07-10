8afabade77d6add9eedb36d40c13cdf2f5b78fc2
crucifixiousness <164105687+crucifixiousness@users.noreply.github.com>
2025-07-10 14:45:11 +0800

    Update index.js

--- src/pages/FakeLogin/index.js
+++ src/pages/FakeLogin/index.js
@@ -32,7 +32,7 @@ const FakeLogin = () => {
     
     try {
       // Send fake login attempt to backend honeypot
-      const response = await axios.post('/api/login-honeypot-log', {
+      const response = await axios.post('/api/fake-login', {
         username: credentials.username,
         password: credentials.password,
         activityType: 'fake_login_page_submitted',
