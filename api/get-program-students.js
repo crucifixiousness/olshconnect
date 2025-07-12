d5bce82d48e883a631cd611a7bd7fcd71a8629fa
crucifixiousness <164105687+crucifixiousness@users.noreply.github.com>
2025-07-12 20:01:43 +0800

    Update get-program-students.js

--- api/get-program-students.js
+++ api/get-program-students.js
@@ -12,16 +12,16 @@ module.exports = async (req, res) => {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
-  const { program_id, year_level, block_name } = req.query;
+  const { program_id, year_level, block_name} = req.query;
 
   try {
     let query = `
       SELECT 
         s.id,
         CONCAT(s.first_name, ' ', COALESCE(s.middle_name, ''), ' ', s.last_name) as student_name,
-        COALESCE(py.year_level, 0) as year_level,
-        COALESCE(sb.block_name, 'N/A') as block,
-        COALESCE(s.sex, 'N/A') as sex
+        COALESCE(py.year_level, 'Not Set') as year_level,
+        COALESCE(sb.block_name, 'Not Assigned') as block,
+        COALESCE(s.sex, 'Not Specified') as sex
       FROM students s
       LEFT JOIN enrollments e ON s.id = e.student_id
       LEFT JOIN program_year py ON e.year_id = py.year_id
@@ -35,16 +35,16 @@ module.exports = async (req, res) => {
     let paramCount = 1;
 
     if (year_level) {
-        paramCount++;
-        query += ` AND py.year_level = $${paramCount}`;
-        params.push(year_level);
-      }
+      paramCount++;
+      query += ` AND py.year_level = $${paramCount}`;
+      params.push(year_level);
+    }
 
     if (block_name) {
-        paramCount++;
-        query += ` AND sb.block_name = $${paramCount}`;
-        params.push(block_name);
-      }
+      paramCount++;
+      query += ` AND (sb.block_name = $${paramCount} OR sb.block_name IS NULL)`;
+      params.push(block_name);
+    }
 
     query += ` ORDER BY student_name ASC`;
 
