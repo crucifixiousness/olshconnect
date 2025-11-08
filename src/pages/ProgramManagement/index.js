8c39c12221d33c56909078247c21ad3f78903465
crucifixiousness <164105687+crucifixiousness@users.noreply.github.com>
2025-11-08 13:59:55 +0800

    Update index.js

--- src/pages/ProgramManagement/index.js
+++ src/pages/ProgramManagement/index.js
@@ -61,6 +61,7 @@ const ProgramManagement = () => {
 
   useEffect(() => {
     fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const fetchData = async () => {
